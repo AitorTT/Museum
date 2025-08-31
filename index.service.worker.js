3731b7ad04d4cf984b2b61fc6f3377b61c086341
AitorTT <aitor.toro.martinez@gmail.com>
2025-08-31 12:49:45 +0200

    change3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756633675|1191652440';
+const CACHE_VERSION = '1756637365|4881761484';
 /** @type {string} */
 const CACHE_PREFIX = 'MUSEUM-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
